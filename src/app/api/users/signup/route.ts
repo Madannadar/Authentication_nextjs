43e047d6f838ea95a3922062220c90d11a9ba98e
Madannadar <madanak0509@gmail.com>
2025-03-28 20:58:34 +0530

    Refactor user signup API and frontend for improved readability and consistency

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,26 +1,26 @@
-import {connect} from '@/dbConfig/dbConfig'
+import { connect } from '@/dbConfig/dbConfig'
 import User from '@/models/userModel'
 import { NextRequest, NextResponse } from 'next/server'
 import bcryptjs from 'bcryptjs'
 
 connect()
 
-export async function POST(request:NextRequest) {
+export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json()
-        const {username, email, password} = reqBody
+        const { username, email, password } = reqBody
         console.log(reqBody);
 
-        const user = await User.findOne({email})
+        const user = await User.findOne({ email })
 
-        if(user){
-            return NextResponse.json({error: "User already exists"}, {status: 400})
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
         }
 
         const salt = await bcryptjs.genSalt(10)
         const hashedPassword = await bcryptjs.hash(password, salt)
 
-        const newUser = new User ({
+        const newUser = new User({
             username,
             email,
             password: hashedPassword
@@ -36,6 +36,6 @@ export async function POST(request:NextRequest) {
         })
 
     } catch (error: any) {
-        return NextResponse.json({error: error.message}, {status: 500})
+        return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
