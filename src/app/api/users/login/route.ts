2189a64b61273be559c114087960cd796c8ee0bd
Madannadar <madanak0509@gmail.com>
2025-03-30 11:07:50 +0530

    Fix typo in password variable and enhance logging in login route

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -8,18 +8,32 @@ connect()
 export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json()
-        const { email, passowrd } = reqBody;
-        console.log(reqBody);
+        const { email, password } = reqBody;
+        console.log('reqBody',reqBody); // reqBody { email: 'abc@gmail.com', password: 'abc123' }
 
         const user = await User.findOne({ email })
+        console.log('user',user); 
+        // user {
+        //     _id: new ObjectId('67e8d442d1cb4017a3126cc2'),
+        //     username: 'abc',
+        //     email: 'abc@gmail.com',
+        //     password: '$2b$10$epZJmmbI2AJ05g0n2W19MemTcpPc3yvxeyBaj0mLxateUUI/T0cCG',
+        //     isVerified: false,
+        //     isAdmin: false,
+        //     __v: 0
+        //   }
+        
         if (!user) {
             return NextResponse.json({ error: "User does not exist" }, { status: 400 })
         }
-
-        const validPassword = await bcryptjs.compare(passowrd, user.password)
+        console.log('user exist'); //user exist
+        
+        const validPassword = await bcryptjs.compare(password, user.password)
         if (!validPassword) {
             return NextResponse.json({ error: "Invalid password" }, { status: 400 })
         }
+        console.log('valid password');
+        
         // create token data 
         const tokenData = {
             id: user._id,
