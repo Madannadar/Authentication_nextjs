a07ac45aed23c7114585db62c1dba6f24809211c
Madannadar <madanak0509@gmail.com>
2025-03-30 19:18:33 +0530

    Add email verification page and API integration

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,10 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
-    const isPublicPath = path == '/login' || path == '/signup'
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
+
     const token = request.cookies.get('token')?.value || ''
+    
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/', request.nextUrl))
         // return NextResponse.redirect('/')
@@ -20,6 +22,7 @@ export const config = {
         '/',
         '/profile/:path*',
         '/login',
-        '/signup'
+        '/signup',
+        '/verifyemail',
     ],
 }
